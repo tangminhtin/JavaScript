7ea62ccea05095c68d35a555f5d9e10656ae7941
tangminhtin <minhtintang@gmail.com>
2020-08-08 23:45:26 +0700

    let, const, operators, el

---
+++ Part_01_Basics_Variables_Data_Types_Operators_And_Functions/lesson_02_let_const_operators_el/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// constant variable
+const defaultResult = 0;
+
+// variable
+let currentResult = defaultResult;
+currentResult = (currentResult + 10) * 3 / 2 -1;
+
+let calcDes = `(${currentResult} + 10) * 3 / 2 -1`;
+
+outputResult(currentResult, calcDes);
